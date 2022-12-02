43c728b14ca95d9b1c2ca9446c60877521149596
Y3drk <ziebura.jedrzej@gmail.com>
2022-12-02 16:11:03 +0100

    spring boot init try

---
+++ SplitWiser/SplitWiser/src/main/java/com/splitwiser/SplitWiser/SplitWiserApplication.java
@@ -0,0 +1,13 @@
+package com.splitwiser.SplitWiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitWiserApplication.class, args);
+	}
+
+}
---
+++ SplitWiser/SplitWiser/src/test/java/com/splitwiser/SplitWiser/SplitWiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwiser.SplitWiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitWiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
