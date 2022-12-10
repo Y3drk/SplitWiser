4948aa0c9ae748f5f31de3f6f1019f6fe659c9ec
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-12-10 06:19:01 +0100

    first model implementation

---
+++ SplitWiser/src/main/java/com/splitwiser/SplitWiser/Group/GroupService.java
@@ -0,0 +1,30 @@
+package com.splitwiser.SplitWiser.Group;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GroupService {
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    public GroupService(){
+    }
+
+    public List<Group> getGroups() {
+        return this.groupRepository.findAll();
+    }
+
+    public Group getGroupById(Long id) {
+        Optional<Group> result =  this.groupRepository.findById(id);
+        return result.orElse(null);
+    }
+
+
+
+}
