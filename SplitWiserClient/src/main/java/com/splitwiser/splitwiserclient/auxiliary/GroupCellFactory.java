cebdc85edbddafddbfc1656df48bddc68b45f5ec
Y3drk <ziebura.jedrzej@gmail.com>
2022-12-19 23:11:46 +0100

    todo text deleted

--- SplitWiserClient/src/main/java/com/splitwiser/splitwiserclient/auxiliary/GroupCellFactory.java
+++ SplitWiserClient/src/main/java/com/splitwiser/splitwiserclient/auxiliary/GroupCellFactory.java
@@ -5,9 +5,6 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.util.Callback;
 
-//TODO: for all cellFactories replace them with cellValueFactories, but
-// "Trzeba tylko zapewnić by model zwracał propertiesy lub tworzyć je w locie, jeśli nie są potrzebne do bindowania"
-
 public class GroupCellFactory implements Callback<ListView<Group>, ListCell<Group>> {
     @Override
     public ListCell<Group> call(ListView<Group> param) {
